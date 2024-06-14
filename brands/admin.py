81fc3dc50dfb8bffa9e9607fa9631184f0d0b31d
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-06-14 10:09:40 -0300

    feat: add brands admin class

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Brand
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', 'description', )
+    
+admin.site.register(Brand, BrandAdmin)
