e7217d9551f57e387c0433870f7c16dd75cbcd7a
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-06-24 09:39:53 -0300

    feat: add admin configs to apps  and change category model options

--- brands/admin.py
+++ brands/admin.py
@@ -4,6 +4,6 @@ from .models import Brand
 
 class BrandAdmin(admin.ModelAdmin):
     search_fields = ('name', )
-    list_display = ('name', 'description', )
+    list_display = ('name', 'created_at', 'updated_at',)
     
 admin.site.register(Brand, BrandAdmin)
