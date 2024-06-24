1fca232602b9f6bc4c942709442530526ddc6e2f
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-06-24 11:48:47 -0300

    feat: add product admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'brand__name', 'category__name',)
+    list_display = ('title', 'brand', 'category', 'serie_number', 'cost_price', 'selling_price', 'quantity', 'created_at', 'updated_at',)
+
+admin.site.register(Product, ProductAdmin)
